518c167996bfdac1c3ba95b971f0b4b428ca3eab
Abdul Hafeez <hafizabdulhafeez240302@gmail.com>
2023-07-19 18:20:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+const app = express();
+const port= 3000;
+
+app.listen(port, ()=> {
+    console.log(`Server started at ${port}`);
+});
